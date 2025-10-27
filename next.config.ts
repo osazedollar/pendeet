b97bccbb398f26b38bde0f20e9b79233bdcc3169
Bumanetwork <victorymosy@gmail.com>
2025-10-27 07:53:22 +0100

    trending error fixed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import { withNextVideo } from "next-video/process";
+//import { withNextVideo } from "next-video/process";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withNextVideo(nextConfig);
+export default (nextConfig);
